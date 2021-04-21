96eda71acb26f1a43803bc427d0c20616277ea91
李祥 <136702480@qq.com>
2021-04-21 11:00:32 +0800

    add

---
+++ MyFramework.framework/Headers/CommonUtility.h
@@ -0,0 +1,16 @@
+//
+//  CommonUtility.h
+//  SearchV3Demo
+//
+//  Created by songjian on 13-8-22.
+//  Copyright (c) 2013年 songjian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CommonUtility : NSObject
+
++ (NSString *)getApplicationScheme;
+
+@end
---
+++ MyFramework.framework/Headers/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by 李祥 on 2021/4/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+#import <MyFramework/CommonUtility.h>
+#import <MyFramework/MethodTool.h>
